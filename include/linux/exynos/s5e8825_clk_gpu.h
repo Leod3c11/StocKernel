50c4eb28c7efa91d9d245bf6d663e5523cb031f9
Leod3c11 <133391551+Leod3c11@users.noreply.github.com>
2025-11-28 16:31:33 -0300

    Update s5e8825_clk_gpu.h
    Signed-off-by: Leod3c11 <133391551+Leod3c11@users.noreply.github.com>

--- include/linux/exynos/s5e8825_clk_gpu.h
+++ include/linux/exynos/s5e8825_clk_gpu.h
@@ -2,7 +2,7 @@
 #ifndef S5E8825_CCLK_GPU_H
 #define S5E8825_CCLK_GPU_H
 
-#define GPU_FREQ_STOCK_KHZ_MAX (897000)
+#define GPU_FREQ_STOCK_KHZ_MAX (800000)
 #ifndef CPU_MAX
 #define CPU_MAX INT_MAX
 #endif
@@ -10,15 +10,15 @@
 /* GPU Clocks */
 #ifdef CONFIG_SOC_S5E8825_GPU_OC // Overclocked frequencies
 /* Max frequency for the GPU */
-#define GPU_FREQ_KHZ_MAX (1001000)
-#define GPU_FREQ_KHZ_MIN (104000)
+#define GPU_FREQ_KHZ_MAX (897000)
+#define GPU_FREQ_KHZ_MIN (156000)
 
-static const unsigned int gpu_custom_clock[] = {1001000, 897000, 806000, 702000, 611000, 507000, 403000, 312000, 208000, 104000};
+static const unsigned int gpu_custom_clock[] = {897000, 800000, 767000, 702000, 572000, 507000, 455000, 377000, 260000, 156000};
 static const unsigned int gpu_custom_min_threshold[] = {85, 82, 80, 79, 78, 70, 60, 50, 30, 0};
 static const unsigned int gpu_custom_max_threshold[] = {100, 95, 93, 90, 87, 85, 80, 70, 60, 40};
 static const unsigned int gpu_custom_staycount[] = {5, 5, 5, 5, 5, 3, 3, 2, 2, 1};
-static const unsigned int gpu_custom_mem_freq[] = {2093000, 1794000, 1539000, 1352000, 1352000, 1014000, 1014000, 845000, 676000, 676000};
-static const unsigned int gpu_custom_lit[] = {1248000, 1056000, 1056000, 1056000, 1056000, 1056000, 0, 0, 0, 0};
+static const unsigned int gpu_custom_mem_freq[] = {2730000, 1716000, 1539000, 1352000, 1352000, 1014000, 1014000, 845000, 676000, 676000};
+static const unsigned int gpu_custom_lit[] = {1300000, 1053000, 1053000, 1053000, 1053000, 1053000, 0, 0, 0, 0};
 static const unsigned int gpu_custom_mid = 0;
 static const unsigned int gpu_custom_big = CPU_MAX;
 
@@ -32,7 +32,7 @@ static const unsigned int gpu_custom_clock[] = {800000, 767000, 702000, 572000, 
 static const unsigned int gpu_custom_min_threshold[] = {78, 78, 78, 78, 70, 60, 50, 30, 0};
 static const unsigned int gpu_custom_max_threshold[] = {100, 95, 95, 95, 90, 80, 70, 60, 40};
 static const unsigned int gpu_custom_staycount[] = {5, 5, 5, 5, 3, 3, 2, 2, 1};
-static const unsigned int gpu_custom_mem_freq[] = {1794000, 1539000, 1352000, 1352000, 1014000, 1014000, 806000, 650000, 650000};
+static const unsigned int gpu_custom_mem_freq[] = {2730000, 1539000, 1352000, 1352000, 1014000, 1014000, 845000, 676000, 676000};
 static const unsigned int gpu_custom_lit[] = {1053000, 1053000, 1053000, 1053000, 1053000, 0, 0, 0, 0};
 static const unsigned int gpu_custom_mid = 0;
 static const unsigned int gpu_custom_big = CPU_MAX;
